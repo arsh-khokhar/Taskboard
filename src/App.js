7847d015f066802f9b83c38270100933fc14e2f8
Arsh <khokharm@myumanitoba.ca>
2021-05-18 22:28:52 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Board from './Board';
+import Login from './Login';
+import {Route, Link} from 'react-router-dom';
+import Navbar from './Navbar';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/board" component={Board} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
